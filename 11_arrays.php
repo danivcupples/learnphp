cb349d0a5d5aa37dcfccdf24947d3708fe5dd042
Dani <danielle.volker@gmail.com>
2018-02-15 15:57:04 -0500

    arrays

--- 11_arrays.php
+++ 11_arrays.php
@@ -5,15 +5,15 @@
     <title>Document</title>
 </head>
 <body>
-<?php 
+<?php
 
 $numberList = array(267,8765,345,'5345', 345, '<h1>Hello</h1>');
 
-//print_r($numberList);
+print_r($numberList);
 
  echo $numberList[5];
-    
-    
+
+
 ?>
 
 </body>
